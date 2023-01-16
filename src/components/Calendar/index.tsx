97183905e5b2e1aad1c7d855bb59c16ebeeead64
Michał Bryk <galeriakoloru90210@gmail.com>
2023-01-16 22:55:48 +0100

    add css to calendar

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react";
+import ReactCalendar from "react-calendar";
+
+interface indexProps {}
+
+const index: FC<indexProps> = ({}) => {
+  return (
+    <div>
+      <ReactCalendar
+        minDate={new Date()}
+        className="REACT-CALENDAR p-2"
+        view="month"
+        onClickDay={(date) => console.log(date)}
+        locale='en-EN'
+      />
+    </div>
+  );
+};
+
+export default index;
