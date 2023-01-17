021cd2d283f4e606d7c04a8eb5c4ab696600bda7
Michał Bryk <galeriakoloru90210@gmail.com>
2023-01-17 09:32:20 +0100

    adding set date

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,18 +1,32 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import ReactCalendar from "react-calendar";
 
 interface indexProps {}
 
+interface DateType {
+  justDate: Date | null
+  dateTime: Date | null
+}
+
 const index: FC<indexProps> = ({}) => {
+  const [ date,setDate ] = useState<DateType>({
+    justDate: null,
+    dateTime: null
+  })
+
   return (
-    <div>
-      <ReactCalendar
+    <div className="h-screen flex flex-col justify-center items-center">
+      {date.justDate ? (
+        <div></div>
+      ) : (
+        <ReactCalendar
         minDate={new Date()}
         className="REACT-CALENDAR p-2"
         view="month"
-        onClickDay={(date) => console.log(date)}
+        onClickDay={(date) => setDate((prev) => ({...prev, justDate: date}))}
         locale='en-EN'
       />
+      )}
     </div>
   );
 };
