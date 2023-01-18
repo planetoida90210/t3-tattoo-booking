764ef55dd625a6071f35d29fcbbc18cfb0c5dfa4
Michał Bryk <galeriakoloru90210@gmail.com>
2023-01-18 21:42:32 +0100

    setting up time to pick from date opening tattoo studio

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,5 +1,7 @@
 import { FC, useState } from "react";
 import ReactCalendar from "react-calendar";
+import { add, format } from 'date-fns';
+
 
 interface indexProps {}
 
@@ -14,10 +16,36 @@ const index: FC<indexProps> = ({}) => {
     dateTime: null
   })
 
+  const getTimes = () => {
+    if(!date.justDate) return
+
+    const {justDate} = date
+
+    const beginning = add(justDate, {hours: 11})
+    const end = add(justDate, {hours: 17})
+    const interval = 30 // in minutes
+
+    const times = []
+
+    for (let i = beginning; i <=  end; i = add(i,{minutes: interval}) ) {
+      times.push(i)
+    }
+
+    return times;
+  }
+
+  const times = getTimes();
+
   return (
     <div className="h-screen flex flex-col justify-center items-center">
       {date.justDate ? (
-        <div></div>
+        <div className="flex gap-4">
+          {times?.map((time,i) => (
+            <div key={`time-${i}`} className="rounded-sm bg-gray-100 p-2">
+              <button type="button" onClick={() => setDate((prev) => ({...prev, dateTime: time}))}>{format(time, 'kk:mm')}</button>
+            </div>
+          ))}
+        </div>
       ) : (
         <ReactCalendar
         minDate={new Date()}
